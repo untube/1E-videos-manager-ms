f1a57ba59c9040b1f8366cb267a8812e89b62f4e
Nicolas <nikolasto2094@gmail.com>
2019-06-03 21:22:51 -0500

    se arregla ruta /subir

--- server.js
+++ server.js
@@ -6,29 +6,21 @@ var bodyParser = require('body-parser');
 const config = require('./db');
 const path = require('path');
 const multer = require('multer');
-
+var name = "";
 let storage = multer.diskStorage({
     destination:(req,file,cb)=>{
         cb(null,'./videos')
     },
     filename:(req,file, cb)=>{
-        cb(null,file.fieldname+"-"+Date.now()+path.extname(file.originalname));
+        name = file.fieldname+"-"+Date.now()+path.extname(file.originalname);
+        cb(null,name);
     }
 })
 const upload = multer({storage:storage});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-
-app.post('/subir',upload.single('file'),(req, res)=>{
-    console.log(req.hostname,req.file.path)
-    return res.send(req.file)
-})
-
-
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
@@ -40,6 +32,35 @@ mongoose.connect(config.DB, function(err, db) {
         console.log('connected!!'+err)
     }
 });
+
+app.post('/subir',upload.single('file'),(req, res)=>{
+
+    console.log(req.hostname,req.file.path)
+
+    var fs = require('fs');
+    var videoFile = fs.readFileSync('./videos/'+name);
+    var video = new Video(req.file);
+    console.log(video);
+    //video["data"]=videoFile;    
+    
+    video.save()
+    .then(item => {
+        //res.send("Video saved to database ");
+        console.log("Video saved to database ")
+    })
+    .catch(err => {
+        console.log("Unable to save to database ")
+        res.status(400).send("Unable to save to database "+err);
+    });
+    Video.find({},function(err,docs){
+        console.log(docs)
+    })
+    return res.send(req.file)
+})
+
+
+
+
 
 var videoSchemaJSON = {
     user_id:Number,
@@ -76,7 +97,7 @@ app.post('/upload',upload.single('file'), function(req, res) {
     var video = new Video(req.file);
     var fs = require('fs');
     console.log(video)
-    var videoFile = fs.readFileSync('./video/'+video.originalname);
+    var videoFile = fs.readFileSync('./videos/'+video.originalname);
     // Convert the video data to a Buffer and base64 encode it.
     var encoded = new Buffer(videoFile).toString('base64');
     video["data"]=encoded;
